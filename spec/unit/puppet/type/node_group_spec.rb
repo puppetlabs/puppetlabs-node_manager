aaea83b546c0e624d6c3cf8f0f0d192402a2f389
Ranjit Viswakumar <whatsaranjit@gmail.com>
2015-04-30 23:18:57 +0000

    Broken unit tests

---
+++ spec/unit/puppet/type/node_group_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Puppet::Type do
+  #Puppet.stubs(:settings).with('localcacert').returns('/etc/puppetlabs/puppet/ssl/certs/ca.pem')
+  type_class = Puppet::Type.type(:node_group)
+  describe type_class do
+
+  before :each do
+    Puppet.settings['localcacert'] = '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
+    Puppet.settings['hostcert']    = '/etc/puppetlabs/puppet/ssl/certs/master.puppetlabs.vm.pem'
+    Puppet.settings['hostprivkey'] = '/etc/puppetlabs/puppet/ssl/private_keys/master.puppetlabs.vm.pem'
+  end
+
+  it 'should not accept an id attribute' do
+    expect {
+      type_class.new({:name => 'asterix', :id => '1'})
+    }.to raise_error /ID is read-only/
+  end
+
+ end
+end
