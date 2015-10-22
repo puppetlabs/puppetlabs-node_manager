a779ba7cb88d60ab7fee43fc783229a78c9cb33d
Jordan Olshevski <jordanolshevski@gmail.com>
2015-10-22 00:38:01 -0400

    Correcting type validation behavior
    The node classifier service doesn't place any constrains on the contents
    of the string provided for a node group's name. I'm removing the
    unnecessary validation from the type. Additionally, the environment
    attribute should support the agent-specified keyword, which falls out of
    the allowed convention. I'm also updating the regex to the same used
    internally.
    
    See the previous commit for the unit tests which unearthed there
    incorrect behaviors: 5adb238e2adcd9090c2df93c0160b99c49bd253d

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -1,12 +1,8 @@
 Puppet::Type.newtype(:node_group) do
-  id_format = /^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$/
   desc 'The node_group type creates and manages node groups for the PE Node Manager'
   ensurable
   newparam(:name, :namevar => true) do
     desc 'This is the common name for the node group'
-    validate do |value|
-      fail("#{name} is not a valid group name") unless value =~ /^[a-zA-Z0-9:\-\_'\s]+$/
-    end
   end
   newproperty(:id) do
     desc 'The ID of the group'
@@ -35,7 +31,7 @@ Puppet::Type.newtype(:node_group) do
     desc 'Environment for this group'
     defaultto :production
     validate do |value|
-      fail("Invalid environment name") unless value =~ /^[a-z][a-z0-9]+$/
+      fail("Invalid environment name") unless value =~ /\A[a-z0-9_]+\Z/ or value == 'agent-specified'
     end
   end
   newproperty(:classes) do
