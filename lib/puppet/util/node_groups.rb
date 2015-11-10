e83365cf31cbf94f687ff446c43938754587d97d
Ranjit Viswakumar <whatsaranjit@gmail.com>
2015-11-10 20:12:28 +0000

    New util and function to pull all or one node_group

---
+++ lib/puppet/util/node_groups.rb
@@ -0,0 +1,38 @@
+require 'puppetclassify'
+require 'yaml'
+
+class Puppet::Util::Node_groups
+  attr_reader :groups
+
+  def initialize
+    auth_info = {
+      "ca_certificate_path" => Puppet.settings['localcacert'],
+      "certificate_path"    => Puppet.settings['hostcert'],
+      "private_key_path"    => Puppet.settings['hostprivkey'],
+    }
+
+    begin
+      nc_settings = YAML.load_file("#{Puppet.settings['confdir']}/classifier.yaml")
+    rescue
+      fail "Could not find file #{Puppet.settings['confdir']}/classifier.yaml"
+    else
+      classifier_url = "https://#{nc_settings['server']}:#{nc_settings['port']}/classifier-api"
+    end
+
+    ng      = PuppetClassify.new(classifier_url, auth_info)
+    @groups = ng.groups.get_groups
+  end
+
+  # Transform the node group array in to a hash
+  # with a key of the name and an attribute
+  # hash of the rest.
+  def self.hashify_group_array(group_array)
+    hashified = Hash.new
+
+    group_array.each do |group|
+      hashified[group['name']] = group
+    end
+
+    hashified
+  end
+end
