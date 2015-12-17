aab31783fc8493be3bb4a34a0cb3e5f6d927be81
Adam Buxton <buxton.adam@gmail.com>
2015-12-17 15:42:59 +0000

    Syntax updated as comments from Zac, not exposed source param at this time due to additional complexity

--- manifests/puppetclassify/install.pp
+++ manifests/puppetclassify/install.pp
@@ -1,6 +1,6 @@
 class node_manager::puppetclassify::install(
-  $version = $node_manager::params::version
-  $gemprovider = $node_manager::params::gemprovider
+  $version      = $node_manager::params::version,
+  $gemprovider  = $node_manager::params::gemprovider
 ) inherits node_manager::params {
 
   package { 'puppetclassify':
