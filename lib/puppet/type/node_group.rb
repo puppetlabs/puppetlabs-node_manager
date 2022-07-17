2667703c5be47b02dd3931afb941e6c51a84f310
benjamin-robertson <72488257+benjamin-robertson@users.noreply.github.com>
2022-07-18 09:54:09 +1000

    Added ability to merge node group rulesets
    * First test of rule merge
    
    * Fix typo
    
    * don't validate
    
    * puts values
    
    * remove first, more puts
    
    * Uniq array
    
    * Check if defined, reorder
    
    * missed?
    
    * Added more output
    
    * Merge rules 1st iteration
    
    * Check b type and size
    
    * check length instead of defined
    
    * Fix logic
    
    * check if values the same
    
    * remove array matching
    
    * array match
    
    * this won't work
    
    * remove super
    
    * Added validae and munge
    
    * check class
    
    * ptus value
    
    * remove validate
    
    * remove munge
    
    * set array match first
    
    * test with insync
    
    * remove sort
    
    * Removed debug puts, replace with comments
    
    * test travis ci
    
    * Updated readme

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -9,7 +9,7 @@ Puppet::Type.newtype(:node_group) do
   end
   newparam(:purge_behavior) do
     desc 'Whether or not to remove data or class parameters not specified'
-    newvalues(:none, :data, :classes, :all)
+    newvalues(:none, :data, :classes, :rule, :all)
     defaultto :all
   end
   newproperty(:id) do
@@ -35,6 +35,45 @@ Puppet::Type.newtype(:node_group) do
   end
   newproperty(:rule, :array_matching => :all) do
     desc 'Match conditions for this group'
+    defaultto []
+    def should
+      case @resource[:purge_behavior]
+      when :rule, :all
+        super
+      else
+        a = shouldorig
+        b = @resource.property(:rule).retrieve || {}
+        # check if the node classifer has any rules defined before attempting merge.
+        if b.length >= 2
+          if b[0] == "or" and b[1][0] == "or" or b[1][0] == "and"
+            # We are merging both rules and pinned nodes
+            rules = (b[1] + a[1].drop(1)).uniq
+            pinned = (a[2,a.length] + b[2,b.length]).uniq
+            b[1] = rules
+            merged = (b + pinned).uniq
+          elsif a[0] == "or" and a[1][0] == "or" or a[1][0] == "and"
+            # We are merging both rules and pinned nodes
+            rules = a[1] # no rules to merge on B side
+            pinned = (a[2,a.length] + b[2,b.length]).uniq
+            merged = (a + pinned).uniq
+          else
+            # We are only doing rules OR pinned nodes
+            merged = (a + b.drop(1)).uniq
+          end
+          if merged == b
+            # values are the same, returning orginal value"
+            b
+          else
+            merged
+          end
+        else
+          a
+        end
+      end
+    end
+    def insync?(is)
+      is == should
+    end
   end
   newproperty(:environment) do
     desc 'Environment for this group'
